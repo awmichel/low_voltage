8cecd895b8a7251e47fc726b2eebc8ec63eacb12
Adam Michel <awmichel90@gmail.com>
2012-12-31 02:37:35 -0500

    Rewrite post finder to not extend HighVoltage::PageFinder.

--- lib/low_voltage/post_finder.rb
+++ lib/low_voltage/post_finder.rb
@@ -1,5 +1,10 @@
 module LowVoltage
-  class PostFinder < HighVoltage::PageFinder
+  class PostFinder
+    VALID_CHARACTERS = "a-zA-Z0-9~!@$%^&*()#`_+-=<>\"{}|[];',?".freeze
+
+    def initialize(post_id)
+      @post_id = post_id
+    end
 
     def find
       Rails.root.join("app/views/#{content_path}/#{clean_path}.md")
@@ -7,6 +12,22 @@ module LowVoltage
 
     def content_path
       LowVoltage.configuration.content_path
+    end
+
+    protected
+
+    # The raw page id passed in by the user
+    attr_reader :post_id
+
+    private
+
+    def clean_path
+      path = Pathname.new("/#{clean_id}")
+      path.cleanpath.to_s[1..-1]
+    end
+
+    def clean_id
+      @post_id.tr("^#{VALID_CHARACTERS}", '')
     end
 
   end
